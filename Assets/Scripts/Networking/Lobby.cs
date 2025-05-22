5e830b3c653955590859635fb4a191f6b0feb7f1
Guilherme <mr.guilhermelimasilva@gmail.com>
2025-05-22 19:45:07 -0300

    Entrar em sala - TODO

---
+++ Assets/Scripts/Networking/Lobby.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Lobby : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField createRoom;
+    public TMP_InputField joinRoom;
+
+    //Metodo ao clicar no Button criar sala 
+    //Todo - configurar numero maximo de players
+    public void CreateRoomButton()
+    {
+        PhotonNetwork.CreateRoom(createRoom.text, new RoomOptions {MaxPlayers = 4}, null);
+    }
+
+    //Metodo ao clicar no button entrar na sala
+    //TODO - add numero de participantes dps
+    public void JoinRoomButton()
+    {
+
+        PhotonNetwork.JoinRoom(joinRoom.text, null);
+    }
+
+    //Metodo ao entrar a sala
+    public override void OnJoinedRoom()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    //Metodo ao dar erro ao entrar na sala
+    //TODO - apagar debug dps
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room Failed" + returnCode + " Message " + message);
+    }
+}
