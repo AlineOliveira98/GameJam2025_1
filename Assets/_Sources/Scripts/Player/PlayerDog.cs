c9da81e28258671c6659a723139e2891fe306bc7
Aline Oliveira <alineboliveira98@gmail.com>
2025-05-24 15:17:04 -0300

    some scripts created

---
+++ Assets/_Sources/Scripts/Player/PlayerDog.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerDog : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    
+    public void Attack()
+    {
+
+    }
+
+    public void Bark()
+    {
+
+    }
+}
---
+++ Assets/_Sources/Scripts/Player/PlayerHuman.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerHuman : MonoBehaviour
+{
+    public void CarryDog()
+    {
+
+    }
+
+    public void DropDog()
+    {
+
+    }
+
+    public void ThrowHook()
+    {
+        
+    }
+}
