affe44542dea01f66e430cde7b97f0645b656075
GuilhermeSV7 <mr.guilhermelimasilva@gmail.com>
2025-05-24 00:11:17 -0300

    Suavizacao de movimento

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 [RequireComponent(typeof(MovementBehaviour))]
 public class Player : MonoBehaviourPun, IPunObservable
@@ -7,17 +8,22 @@ public class Player : MonoBehaviourPun, IPunObservable
     private MovementBehaviour movement;
     private Vector2 clientPos;
     private Vector2 velocityCache;
+    private Rigidbody2D rb;
+    private Vector2 targetPosition;
+    private Vector2 targetVelocity;
+
 
     void Awake()
     {
         movement = GetComponent<MovementBehaviour>();
+        rb = movement.GetRigidbody();
 
-        // Desativa o script de movimento dos jogadores remotos
         if (!photonView.IsMine)
         {
             movement.enabled = false;
         }
     }
+
 
     void Update()
     {
@@ -52,27 +58,33 @@ public class Player : MonoBehaviourPun, IPunObservable
     }
 
 
+    [SerializeField] private float smoothSpeed = 10f;
+
     private void SmoothMovement()
     {
-        transform.position = Vector2.MoveTowards(transform.position, clientPos, Time.deltaTime * 10f);
+        // Move suavemente o objeto até a posição recebida
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * smoothSpeed);
     }
+
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        Rigidbody2D rig = movement.GetRigidbody();
-
         if (stream.IsWriting)
         {
-            stream.SendNext(rig.position);
-            stream.SendNext(rig.linearVelocity);
+            // Enviando posição e velocidade para os outros
+            stream.SendNext(transform.position);
+            stream.SendNext(rb.linearVelocity);
         }
         else
         {
-            clientPos = (Vector2)stream.ReceiveNext();
-            velocityCache = (Vector2)stream.ReceiveNext();
+            // Recebendo do dono
+            targetPosition = (Vector2)stream.ReceiveNext();
+            targetVelocity = (Vector2)stream.ReceiveNext();
 
+            // Lag compensation
             float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
-            clientPos += velocityCache * lag;
+            targetPosition += targetVelocity * lag;
         }
     }
+
 }
