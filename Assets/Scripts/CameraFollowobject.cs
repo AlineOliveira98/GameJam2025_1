8975e5ac0e5aee58caa09e1c7dd5fb997a5ba78d
GuilhermeSV7 <mr.guilhermelimasilva@gmail.com>
2025-05-25 21:04:04 -0300

    Version 2.0 cinemachine

---
+++ Assets/Scripts/CameraFollowobject.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraFollowobject : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float lateralOffset = 3f;
+    [SerializeField] private float verticalOffset = 0.5f;
+    [SerializeField] private float smoothTime = 0.2f;
+
+    private Vector3 velocity;
+    private bool isFacingRight = true;
+
+    public void SetTarget(Transform t) => target = t;
+    public void SetFacing(bool facingRight) => isFacingRight = facingRight;
+
+    void Update()
+    {
+        if (target == null) return;
+
+        float direction = isFacingRight ? 1f : -1f;
+        Vector3 offset = new Vector3(direction * lateralOffset, verticalOffset, 0);
+        Vector3 targetPos = target.position + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
